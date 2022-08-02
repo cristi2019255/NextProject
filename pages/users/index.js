9b91661ba0e3d7019c56f4f28b30b7a525b29b53
cristi2019255 <cristi.grosu.2013@mail.ru>
2022-08-02 14:02:08 +0200

    refactoring

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import MainContainer from "../components/MainContainer";
+import MainContainer from "../../components/MainContainer";
 
 const Users = ({ users }) => {
   return (
